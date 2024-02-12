2196d146f15d11d69761b5e963cbe3bf9424b615
drew verlee <drew.verlee@gmail.com>
2024-02-12 11:52:09 -0600

    add a way to get to kafkaconsumer from javadocs function

--- src/core.clj
+++ src/core.clj
@@ -3,3 +3,8 @@
 (import '(org.apache.kafka.clients.consumer KafkaConsumer))
 
 KafkaConsumer
+
+
+(clojure.java.javadoc/add-remote-javadoc "org.apache.kafka" "https://kafka.apache.org/20/javadoc/")
+
+(clojure.java.javadoc/javadoc KafkaConsumer)
